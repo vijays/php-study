3c65feaeb6d5434d60a30b9ed07bae5d0966918a
vijays <vijays@users.noreply.github.com>
2017-10-01 20:57:25 +0530

    Simple class
    Simple class

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE <html>
+<html>
+<body>
+    <?php
+        //Simple class
+        class c1 {
+            function greet() {
+                echo "Hello PHP" ;            
+            }
+        }
+        $myObj = new c1 ;
+        $myObj->greet() ;
+    ?>
+</body>    
+</html>
