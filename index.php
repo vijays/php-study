22e346e4df13de3e9f5b8c8112076d0e7c999934
vijays <vijays@users.noreply.github.com>
2017-10-02 19:05:55 +0530

    Dependency Injection

--- index.php
+++ index.php
@@ -5,11 +5,39 @@
         //Simple class
         class c1 {
             function greet() {
-                echo "Hello PHP" ;            
+                echo "Hello PHP <p>";
             }
         }
         $myObj = new c1 ;
         $myObj->greet() ;
+
+        // Dependency Injection
+        class logger {
+            function log($msg){
+                echo "log: $msg";
+            }
+        }
+
+        class userOperation {
+
+            private $logger;
+
+            public function __CONSTRUCT(logger $logger) {
+                $this->logger = $logger;
+            }
+
+            public function createUser() {
+                $this->logger->log("In create user.");
+            }
+        }
+
+        $objLogger = new logger();
+
+        $objUserOperation = new userOperation($objLogger);
+
+        $objUserOperation->createUser();
+        
+
     ?>
 </body>    
 </html>
