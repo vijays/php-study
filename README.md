b48a524da8c9857290861c076fd8563729e7356d
vijays <vijays@users.noreply.github.com>
2017-10-02 19:36:12 +0530

    Traits

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PHP
 Learning PHP
+
+- Simple Class
+- Dependency Injection
+- Traits
+
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         // Dependency Injection
         class logger {
             function log($msg){
-                echo "log: $msg";
+                echo "log: $msg <p>";
             }
         }
 
@@ -36,8 +36,52 @@
         $objUserOperation = new userOperation($objLogger);
 
         $objUserOperation->createUser();
-        
 
-    ?>
+        //Traits
+
+        class vehicle {
+            public function seat() {
+                echo "Seated <br>";
+            }
+        }
+
+        trait moveBicycle {
+            public function pedal() {
+                echo "Pedalling <br>";
+            }
+        }
+
+        trait moveCar {
+            public function accelerate() {
+                echo "Accelerated <br>";
+            }
+        }
+
+        trait brake {
+            public function brake() {
+                echo "Applied brakes <br>";
+            }
+        }
+
+        class bicycle extends vehicle {
+            use moveBicycle, brake;
+        }
+
+        class car extends vehicle {
+            use moveCar, brake;
+        }
+
+        $objBicycle = new bicycle;
+        $objCar = new car;
+
+        $objBicycle->seat();
+        $objBicycle->pedal();
+        $objBicycle->brake(); 
+
+        $objCar->seat();
+        $objCar->accelerate();
+        $objCar->brake();
+        
+?>
 </body>    
 </html>
