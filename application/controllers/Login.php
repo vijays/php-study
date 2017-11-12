08baa204b847ca3f1e14f7ec2562cbd2ae986bd4
Vijay <vijay@Vijays-MacBook-Air.local>
2017-11-12 10:20:54 +0530

    Login model and database validation

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,13 +10,28 @@ class Login extends MY_Controller {
     public function admin_login(){
       $this->load->library('form_validation');
 
-      $this->form_validation->set_rules('uid','User Id','required|alpha|min_length[5]|max_length[8]|is_unique[users.uid]');
+      $this->form_validation->set_rules('uid','User Id','required|alpha|min_length[5]|max_length[8]');
       $this->form_validation->set_rules('pwd','Password','required');
 
       $this->form_validation->set_error_delimiters("<p class='text-danger'>","</p>");
 
       if ( $this->form_validation->run() ){
         //Success
+        $userid = $this->input->post('uid');
+        $passwd = $this->input->post('pwd');
+        echo "userid: $userid and passwd: $passwd";
+
+        $this->load->model('Loginmodel');
+
+        if ($this->Loginmodel->login_validation($userid, $passwd))
+        {
+          //success
+          echo "Password match";
+        }
+        else {
+          //failed
+          echo "Password does not match";
+        }
       }
       else{
         //Fail
