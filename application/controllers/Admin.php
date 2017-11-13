41548bf970b83721a60a3abab62e2b90fa21873d
vijays <vijays@users.noreply.github.com>
2017-11-13 17:05:53 +0530

    Check if user is loggedin before displaying dashboard

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,11 +2,19 @@
 
 class Admin extends MY_Controller {
 
+  public function __construct(){
+    parent::__construct();
+
+    if ( !$this->session->userdata('user_id') )
+      return redirect('Login');
+  }
+
     public function index() {
         echo "in Admin";
     }
 
     public function dashboard(){
+
       $this->load->model('Articlesmodel', 'articles');
       $articleslist = $this->articles->list_articles();
 
